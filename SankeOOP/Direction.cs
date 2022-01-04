a832b52415a7008d95d0a6c98d7805f84d4d2226
Opilane TTHK <opilane@tthk.ee>
2022-01-04 12:04:46 +0200

    yes

---
+++ SankeOOP/Direction.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SankeOOP
+{
+    enum Direction
+    {
+        LEFT,
+        RIGHT,
+        UP,
+        DOWN
+    }
+}
